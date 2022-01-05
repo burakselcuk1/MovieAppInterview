9cf15789c155c0980b74cb6ab845eacbfa0ea862
burak <brkdewils@gmail.com>
2022-01-05 18:18:51 +0300

    api interface created!

--- app/src/main/java/com/example/movieappinterview/api/MovieDbInterface.kt
+++ app/src/main/java/com/example/movieappinterview/api/MovieDbInterface.kt
@@ -1,4 +1,14 @@
 package com.example.movieappinterview.api
 
+import com.example.movieappinterview.model.movie
+import retrofit2.http.GET
+
 interface MovieDbInterface {
+
+    //https://api.themoviedb.org/3/movie/popular?api_key=6eeb39d6df396b3373f188208038112c
+
+    @GET("/3/movie/popular?api_key=bbf5a3000e95f1dddf266b5e187d4b21")
+    fun getMovieList(): retrofit2.Call<movie>
+
+
 }
