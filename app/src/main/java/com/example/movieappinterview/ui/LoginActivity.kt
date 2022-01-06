77e04c56e9899c7aaddf542c33859aefb6a588fe
burak <brkdewils@gmail.com>
2022-01-06 17:49:35 +0300

    LoginActivity.kt updated!

--- app/src/main/java/com/example/movieappinterview/ui/LoginActivity.kt
+++ app/src/main/java/com/example/movieappinterview/ui/LoginActivity.kt
@@ -3,6 +3,8 @@ package com.example.movieappinterview.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
 import com.example.movieappinterview.R
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
@@ -25,9 +27,50 @@ class LoginActivity : AppCompatActivity() {
             val intent = Intent(this,RegisterActivity::class.java)
             startActivity(intent)
         }
+
+        loginButton.setOnClickListener {
+            singIn()
+        }
+    }
+
+    private fun singIn() {
+
+        val loginemail = login_email.text.toString().trim()
+        val loginPassword = login_password.text.toString().trim()
+
+
+        if(loginemail.isEmpty()){
+            Toast.makeText(this,"E-mail can not be empty!", Toast.LENGTH_SHORT).show()
+        }
+        else if(!Patterns.EMAIL_ADDRESS.matcher(loginemail).matches()){
+            Toast.makeText(this,"Invalid E-mail adress!", Toast.LENGTH_SHORT).show()
+
+        }
+        else if(loginPassword.isEmpty()){
+            Toast.makeText(this,"Password can not be empty!", Toast.LENGTH_SHORT).show()
+        }
+        else{
+            firebaseAuth.signInWithEmailAndPassword(loginemail,loginPassword).addOnSuccessListener {
+                val intent = Intent(this,MainActivity::class.java)
+                startActivity(intent)
+            }.addOnFailureListener{
+                Toast.makeText(this,"User didn't find!", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+
+
     }
 
     private fun checkUser() {
-        TODO("Not yet implemented")
+
+        val firebase = firebaseAuth.currentUser
+        if (firebase != null){
+            Toast.makeText(this,"Welcome", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
