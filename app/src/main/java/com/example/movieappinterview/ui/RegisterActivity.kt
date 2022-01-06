d62287adab319e2b917f4628bdc11c8e1b3405ae
burak <brkdewils@gmail.com>
2022-01-06 17:05:12 +0300

    RegisterActivity.kt created!

---
+++ app/src/main/java/com/example/movieappinterview/ui/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.movieappinterview.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.movieappinterview.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
