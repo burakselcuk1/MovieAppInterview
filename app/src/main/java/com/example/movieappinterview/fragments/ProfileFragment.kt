52d7ebbb56f62e643850d35ac05e35c747dc2d33
burak <brkdewils@gmail.com>
2022-01-06 18:10:56 +0300

    implamentation files added for firebase realtime database!

--- app/src/main/java/com/example/movieappinterview/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/movieappinterview/fragments/ProfileFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.movieappinterview.R
+import com.google.firebase.auth.FirebaseAuth
+
 
 
 class ProfileFragment : Fragment() {
