aa1ffb1744005e2f1cf53555f8f4528197f0d850
burak <brkdewils@gmail.com>
2022-01-06 17:40:20 +0300

    activity_login.xml updated!

--- app/src/main/java/com/example/movieappinterview/ui/LoginActivity.kt
+++ app/src/main/java/com/example/movieappinterview/ui/LoginActivity.kt
@@ -4,17 +4,30 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.movieappinterview.R
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
+
+        private lateinit var firebaseAuth: FirebaseAuth
+
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        //  Checks if already logged in
+        checkUser()
+
         //Goes to register.xml for create new account
         registerTextview.setOnClickListener {
             val intent = Intent(this,RegisterActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun checkUser() {
+        TODO("Not yet implemented")
     }
 }
