ab9e7d143bb8a02c4bd31016cb151d94ea311dae
burak <brkdewils@gmail.com>
2022-01-07 17:55:59 +0300

    SavedMovieViewModel.kt updated!

--- app/src/main/java/com/example/movieappinterview/ui/SavedMovieViewModel.kt
+++ app/src/main/java/com/example/movieappinterview/ui/SavedMovieViewModel.kt
@@ -1,4 +1,21 @@
 package com.example.movieappinterview.ui
 
-class SavedMovieViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.example.db.MovieDatabase
+import com.example.db.MovieRepository
+import com.example.movieappinterview.model.Result
+
+class SavedMovieViewModel(application: Application): AndroidViewModel(application) {
+
+    private val readAllData: LiveData<List<Result>>
+    private val repository: MovieRepository
+    init {
+        val movieDao = MovieDatabase.getDatabase(application).movieDao()
+        repository =MovieRepository(movieDao)
+        readAllData = repository.readAllData
+
+    }
+
 }
