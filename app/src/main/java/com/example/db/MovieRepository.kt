b13371a5dd510f10c7dfbd53b30c40e5a0d9010e
burak <brkdewils@gmail.com>
2022-01-08 01:43:29 +0300

    MovieRepository.kt updated Log.e added for check data

--- app/src/main/java/com/example/db/MovieRepository.kt
+++ app/src/main/java/com/example/db/MovieRepository.kt
@@ -1,13 +1,18 @@
 package com.example.db
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import com.example.movieappinterview.model.Result
 
 class MovieRepository(private val movieDao: Dao) {
 
+    val TAG: String = "TAG_MovieRepository"
+
+
     val readAllData: LiveData<List<Result>> = movieDao.readAllData()
 
     suspend fun addMovie(movie: Result){
+        Log.e(TAG,"girdi-movie2")
         movieDao.addMovie(movie)
     }
 
