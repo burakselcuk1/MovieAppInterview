5c714488d632e923d8cba24c14eab0a0a4e5406e
burak <brkdewils@gmail.com>
2022-01-07 17:36:45 +0300

    Dao updated!

--- app/src/main/java/com/example/db/Dao.kt
+++ app/src/main/java/com/example/db/Dao.kt
@@ -1,7 +1,14 @@
 package com.example.db
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import com.example.movieappinterview.model.Result
 
 @Dao
 interface Dao {
+
+    //Add new movie into to room db
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun addMovie(movie : Result)
 }
