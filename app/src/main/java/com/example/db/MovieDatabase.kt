52cd0462c173a4b669148e4c19d22a844d60ff62
burak <brkdewils@gmail.com>
2022-01-07 17:44:56 +0300

    MovieDatabase.kt uptaded!

--- app/src/main/java/com/example/db/MovieDatabase.kt
+++ app/src/main/java/com/example/db/MovieDatabase.kt
@@ -1,6 +1,34 @@
 package com.example.db
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
+@Database(entities = [Result::class], version = 1, exportSchema = false)
 abstract class MovieDatabase: RoomDatabase() {
+
+    abstract fun movieDao(): Dao
+
+    companion object{
+        @Volatile
+        private var INSTANCE: MovieDatabase? = null
+
+        fun getDatabase(context: Context): MovieDatabase{
+            val tempInstance = INSTANCE
+            if (tempInstance!=null){
+                return tempInstance
+            }
+            synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    MovieDatabase::class.java,
+                    "movie_data"
+                ).build()
+                INSTANCE =instance
+                return instance
+            }
+        }
+
+    }
 }
