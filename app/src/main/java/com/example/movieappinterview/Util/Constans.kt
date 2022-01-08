218fdf33993cb88773ba0dc792d62e0e88655475
burak <brkdewils@gmail.com>
2022-01-08 17:52:43 +0300

    Constans.kt updated

--- app/src/main/java/com/example/movieappinterview/Util/Constans.kt
+++ app/src/main/java/com/example/movieappinterview/Util/Constans.kt
@@ -2,7 +2,7 @@ package com.example.movieappinterview.Util
 
 class Constans {
     companion object {
-        const  val API_KEY = "69462fec11f14968b8da9a8bb59f4dbc"
+        const  val API_KEY = "6eeb39d6df396b3373f188208038112c"
         const val BASE_URL ="https://api.themoviedb.org/"
         const val SEARCH_NEWS_TIME_DELAY = 500L
         const val QUERY_PAGE_SIZE = 20
