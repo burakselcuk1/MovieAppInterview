349304e00d07e6501c1726fa1bb204e5069c3a83
burak <brkdewils@gmail.com>
2022-01-09 22:45:16 +0300

    SavedFragment.kt reviewed for error

--- app/src/main/java/com/example/movieappinterview/fragments/SavedFragment.kt
+++ app/src/main/java/com/example/movieappinterview/fragments/SavedFragment.kt
@@ -21,6 +21,7 @@ import com.example.movieappinterview.viewmodel.SavedMovieViewModel
 import kotlinx.android.synthetic.main.fragment_movie_details.*
 import kotlinx.android.synthetic.main.fragment_saved.*
 import kotlinx.android.synthetic.main.single_room_item.*
+import kotlinx.android.synthetic.main.single_room_item.view.*
 import java.io.Serializable
 
 
@@ -30,16 +31,19 @@ class SavedFragment : Fragment() {
     private lateinit var roomAdater: RoomAdapter
     private lateinit var singleMovieData:Result
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
+        savedMovieViewModel = ViewModelProvider(this).get(SavedMovieViewModel::class.java)
 
-        singleMovieData = requireArguments().getSerializable("movie") as Result
-        singleMovieData.let {
-
-        }
 
         return inflater.inflate(R.layout.fragment_saved, container, false)
     }
@@ -47,28 +51,35 @@ class SavedFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        savedMovieViewModel = ViewModelProvider(this).get(SavedMovieViewModel::class.java)
+
+        if(arguments?.get("movie")!= null) {
+            if (requireArguments().getSerializable("movie") != null) {
+                singleMovieData = requireArguments().getSerializable("movie") as Result
+
+                val saveMovie = Result(singleMovieData.adult,
+                    singleMovieData.backdrop_path,
+                    singleMovieData.id,
+                    singleMovieData.original_language,
+                    singleMovieData.original_title,
+                    singleMovieData.overview,
+                    singleMovieData.popularity,
+                    singleMovieData.poster_path,
+                    singleMovieData.release_date,
+                    singleMovieData.original_title,
+                    singleMovieData.video,
+                    singleMovieData.vote_average,
+                    singleMovieData.vote_count)
+
+
+                savedMovieViewModel.addMovie(saveMovie)
+            }
+        }
+
         // Setup saved_movie_recyclerview to Movie Adapter
         saved_movie_recyclerview.layoutManager = LinearLayoutManager(context)
         // Setup savedMovieViewModel to SavedViewModel
-        savedMovieViewModel = ViewModelProvider(this).get(SavedMovieViewModel::class.java)
 
-
-        val saveMovie = Result(singleMovieData.adult,
-            singleMovieData.backdrop_path,
-            singleMovieData.id,
-            singleMovieData.original_language,
-            singleMovieData.original_title,
-            singleMovieData.overview,
-            singleMovieData.popularity,
-            singleMovieData.poster_path,
-            singleMovieData.release_date,
-            singleMovieData.original_title,
-            singleMovieData.video,
-            singleMovieData.vote_average,
-            singleMovieData.vote_count)
-
-
-        savedMovieViewModel.addMovie(saveMovie)
 
         //Read all room db data and past to recyclerview
         savedMovieViewModel.readAllData.observe(viewLifecycleOwner, Observer {
