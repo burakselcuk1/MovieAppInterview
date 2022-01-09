079d9dd0e435f95e93ef434459d070d7e84301af
burak <brkdewils@gmail.com>
2022-01-09 19:05:25 +0300

    Constans.kt updated for val POSTER_MAIN_URL

--- app/src/main/java/com/example/movieappinterview/Util/Constans.kt
+++ app/src/main/java/com/example/movieappinterview/Util/Constans.kt
@@ -6,5 +6,6 @@ class Constans {
         const val BASE_URL ="https://api.themoviedb.org/"
         const val SEARCH_NEWS_TIME_DELAY = 500L
         const val QUERY_PAGE_SIZE = 20
+        const val POSTER_MAIN_URL= "https://image.tmdb.org/t/p/w342"
     }
 }
