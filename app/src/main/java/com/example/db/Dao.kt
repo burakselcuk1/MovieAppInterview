d8cc47a0a9739ebea93c2c90879b312082e07210
burak <brkdewils@gmail.com>
2022-01-09 19:43:20 +0300

    added delete fun

--- app/src/main/java/com/example/db/Dao.kt
+++ app/src/main/java/com/example/db/Dao.kt
@@ -1,10 +1,8 @@
 package com.example.db
 
 import androidx.lifecycle.LiveData
+import androidx.room.*
 import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
 import com.example.movieappinterview.model.Result
 
 @Dao
@@ -12,6 +10,9 @@ interface Dao {
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun addMovie(movie : Result)
+
+    @Delete
+    suspend fun deleteMovie(movie: Result)
 
     @Query("SELECT * FROM movie_data ORDER BY id ASC")
      fun readAllData(): LiveData<List<Result>>
