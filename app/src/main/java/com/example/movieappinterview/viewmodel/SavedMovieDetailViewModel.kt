bf1761c5e58a31e1186c32c4d47bfba0d22d4a49
burak <brkdewils@gmail.com>
2022-01-13 01:05:23 +0300

    SavedMovieDetailViewModel.kt created for display room data detail on screen

---
+++ app/src/main/java/com/example/movieappinterview/viewmodel/SavedMovieDetailViewModel.kt
@@ -0,0 +1,49 @@
+package com.example.movieappinterview.viewmodel
+
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.db.MovieRepository
+import com.example.movieappinterview.api.MovieDbApi
+import com.example.movieappinterview.model.Result
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class SavedMovieDetailViewModel(application: Application): AndroidViewModel(application) {
+
+    private val apiService = MovieDbApi()
+    private val disposable = CompositeDisposable()
+
+    val moviesDetail = MutableLiveData<Result>()
+
+
+
+    fun getMoviesDetailForSavedDetail(movieId: String){
+        disposable.addAll(
+            apiService.getMovieDetails(movieId).subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(object : DisposableSingleObserver<Result>(){
+
+                    override fun onSuccess(t: Result) {
+                        moviesDetail.value = t
+                        //Log.e("burak", "başarılı" + t.toString())
+                    }
+
+                    override fun onError(e: Throwable) {
+                        Log.e("burak", "başarısız "+e.toString())
+                        e.printStackTrace()
+                    }
+
+                })
+        )
+    }
+
+
+    }
