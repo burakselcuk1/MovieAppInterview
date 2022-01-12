c737e09de6194c45834fde5ca08d8f6dd53744af
burak <brkdewils@gmail.com>
2022-01-13 01:06:52 +0300

    SavedFragment.kt reviewed

--- app/src/main/java/com/example/movieappinterview/fragments/SavedFragment.kt
+++ app/src/main/java/com/example/movieappinterview/fragments/SavedFragment.kt
@@ -33,8 +33,6 @@ class SavedFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
     }
 
     override fun onCreateView(
