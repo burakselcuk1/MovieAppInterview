66a734d1ad00ec22911a84b15557dfcc3128ec14
burak <brkdewils@gmail.com>
2022-01-13 17:15:09 +0300

    MovieRepository.kt reviewed!

--- app/src/main/java/com/example/db/MovieRepository.kt
+++ app/src/main/java/com/example/db/MovieRepository.kt
@@ -6,20 +6,14 @@ import com.example.movieappinterview.model.Result
 
 class MovieRepository(private val movieDao: Dao) {
 
-
-
-
     val readAllData: LiveData<List<Result>> = movieDao.readAllData()
-
 
      // Add a new movie to RoomDB
     suspend fun addMovie(movie: Result){
         movieDao.addMovie(movie)
     }
-
     //Delete a single movie from RoomDb
     suspend fun deleteMovieFromRoomDb(movie: Result){
         movieDao.deleteMovie(movie)
     }
-
 }
