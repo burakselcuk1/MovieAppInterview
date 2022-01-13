33f7175ed974448de2545a5c3c600d95626d58d4
burak <brkdewils@gmail.com>
2022-01-14 00:35:11 +0300

    SavedMovieDetailViewModel.kt updated for delete movie from room database

--- app/src/main/java/com/example/movieappinterview/fragments/SavedMovieDetailFragment.kt
+++ app/src/main/java/com/example/movieappinterview/fragments/SavedMovieDetailFragment.kt
@@ -5,13 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.example.movieappinterview.R
+import com.example.movieappinterview.Util.Constans.Companion.POSTER_MAIN_URL
 import com.example.movieappinterview.model.Result
 import com.example.movieappinterview.viewmodel.MovieDetailViewModel
 import com.example.movieappinterview.viewmodel.SavedMovieDetailViewModel
+import com.example.movieappinterview.viewmodel.SavedMovieViewModel
 import kotlinx.android.synthetic.main.fragment_movie_details.*
 import kotlinx.android.synthetic.main.fragment_saved_movie_detail.*
 
@@ -41,6 +45,17 @@ class SavedMovieDetailFragment : Fragment() {
         savedMovieDetailViewModel = ViewModelProvider(this).get(SavedMovieDetailViewModel::class.java)
         savedMovieDetailViewModel.getMoviesDetailForSavedDetail(movieId!!)
         getMoviesDetailFromApi()
+
+        delete_saved_movie.setOnClickListener {
+            deleteMovieFromRoomDb()
+        }
+    }
+
+    private fun deleteMovieFromRoomDb() {
+        savedMovieDetailViewModel.moviesDetail.observe(viewLifecycleOwner, Observer {
+            savedMovieDetailViewModel.deleteMovie(it)
+            findNavController().navigate(R.id.action_savedMovieDetailFragment_to_savedFragment)
+        })
     }
 
     // Get movies detail from api for saved movie detail screen
@@ -48,14 +63,13 @@ class SavedMovieDetailFragment : Fragment() {
 
         savedMovieDetailViewModel.moviesDetail.observe(viewLifecycleOwner, Observer{
             //set values into detailFragment components
-
             it.let {
                 resultMovie = it
 
                 saved_movie_detail_title.text = resultMovie.original_title
                 saved_movie_detail_tagline.text = resultMovie.overview
-                val moviePosterUrl= "https://image.tmdb.org/t/p/w342/" + resultMovie.poster_path
-                val movieBackDropPath ="https://image.tmdb.org/t/p/w342/" + resultMovie.backdrop_path
+                val moviePosterUrl= POSTER_MAIN_URL + resultMovie.poster_path
+                val movieBackDropPath = POSTER_MAIN_URL + resultMovie.backdrop_path
                 Glide.with(this)
                     .load(moviePosterUrl)
                     .into(saved_movie_detail_poster)
