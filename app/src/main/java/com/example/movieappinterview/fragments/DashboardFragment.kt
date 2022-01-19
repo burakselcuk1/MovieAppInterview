558598fdaf751b784137224194aa5873283829cc
burak <brkdewils@gmail.com>
2022-01-19 20:34:08 +0300

    DashboardFragment.kt reviewed!

--- app/src/main/java/com/example/movieappinterview/fragments/DashboardFragment.kt
+++ app/src/main/java/com/example/movieappinterview/fragments/DashboardFragment.kt
@@ -25,10 +25,8 @@ class DashboardFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_dashboard, container, false)
     }
-
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -47,10 +45,7 @@ class DashboardFragment : Fragment() {
                 recyclerView.visibility = View.VISIBLE
                 movieAdapter = MovieAdapter( movies!!)
                 recyclerView.adapter = movieAdapter
-
-                //    adapter.updataMovieList(it)
             }
         })
     }
-
 }
