2d38492ec21b2690f36da93767920663d8bb9bae
burak <brkdewils@gmail.com>
2022-01-19 20:16:09 +0300

    SavedMovieViewModel.kt reviewed!

--- app/src/main/java/com/example/movieappinterview/viewmodel/SavedMovieViewModel.kt
+++ app/src/main/java/com/example/movieappinterview/viewmodel/SavedMovieViewModel.kt
@@ -1,19 +1,15 @@
 package com.example.movieappinterview.viewmodel
 
 import android.app.Application
-import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
-import com.example.db.MovieDatabase
-import com.example.db.MovieRepository
+import com.example.movieappinterview.db.MovieDatabase
+import com.example.movieappinterview.db.MovieRepository
 import com.example.movieappinterview.api.MovieDbApi
 import com.example.movieappinterview.model.Result
-import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.observers.DisposableSingleObserver
-import io.reactivex.schedulers.Schedulers
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
